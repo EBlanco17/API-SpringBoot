ff21c3cc86d3ecbd4a41ff0e4128a6535c98aa91
EMERSON BLANCO <e.blanco017@gmail.com>
2023-09-06 10:08:12 -0500

    Se agrega funcionalidades para paciente

---
+++ src/main/java/med/volt/api/domain/paciente/DatosRegistroPaciente.java
@@ -0,0 +1,27 @@
+package med.volt.api.domain.paciente;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import med.volt.api.domain.direccion.DatosDireccion;
+
+public record DatosRegistroPaciente(
+        @NotBlank
+        String nombre,
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String telefono,
+
+        //@Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")
+        @NotBlank
+        String documento,
+
+        @NotNull
+        @Valid
+        DatosDireccion direccion) {
+}
